451e3fce355e6a946b307ec5f8c51e243f5b5c03
Kuhlman <rkuhlman813@student.brazosportisd.net>
2020-12-01 11:11:17 -0600

    Blocked out input form stuff

---
+++ script.js
@@ -0,0 +1,37 @@
+var xhr = new XMLHttpRequest();
+
+let targetUrl = document.getElementById("url").innerHTML;  
+
+// True is for async
+xhr.open("POST", targetUrl, true);
+
+// going to have to add each one separately
+xhr.setRequestHeader('Content-Type', 'application/json');
+
+
+let data = {
+    // Some base data is the same for everyone
+    // Generate session
+};
+
+// add in the custom data from the inputs
+
+xhr.send(JSON.stringify(data));
+
+
+
+
+xhr.onreadystatechange = function () {
+    if (this.readyState != 4) return;
+
+    if (this.status == 200) {
+        displaySuccess();
+
+        // we get the returned data
+    } else {
+        console.log(this.status);
+        displayFail();
+    }
+
+    // end of state change: it can be after some time (async)
+};
